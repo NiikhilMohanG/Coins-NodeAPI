92ff7f62794654632dde41ec29a2dfc683c2a673
NiikhilMohanG <nikhil.g.mohan@outlook.com>
2019-03-13 11:32:59 +0530

    renamed base to shared

--- controllers/home.js
+++ controllers/home.js
@@ -3,6 +3,8 @@ var base = require("../libs/base.js");
 
 var app = base.Express;
 
-app.get('/hello', function (req, res) {
-    res.send("listening to port " + base.Settings.port);
+//app.set('view engine', 'jade');
+
+app.get('/', function (req, res) {
+    res.send("listening to port " + base.Settings.port);    
 }).listen(base.Settings.port);
